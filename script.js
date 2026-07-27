43603857268ad6f6495d6d2885a8d60eb180e782
user <usernameqwert3@gmail.com>
2026-07-28 02:13:00 +0400

    added deadline input and deadline text for list elements and added checkbox for toggle(ui part only)

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
 let input = document.getElementById("text");
 let addBtn = document.getElementById("addBtn");
 let list = document.getElementById("list");
+let dateInput=document.getElementById("dateInput");
+let form=document.querySelector('form')
 
 function main() {
-  if (!input.value.trim()) {
-    return;
-  }
 
-  makingTask(input.value);
+  makingTask(input.value,dateInput.value);
 
   input.value = "";
 
-  saveLocalStorage();
+  // saveLocalStorage();
 }
 
-window.addEventListener("load", () => {
-  JSON.parse(localStorage.getItem("items")).forEach((item) => makingTask(item));
-});
+// window.addEventListener("load", () => {
+//   JSON.parse(localStorage.getItem("items")).forEach((item) => makingTask(item));
+// });
 
-function makingTask(text) {
+function makingTask(text,date) {
+  if(!text || !date){setError(text,date);return;}
   let listElement = document.createElement("li");
 
-  listElement.innerHTML = `${text} <i class="fa-solid fa-trash" style='font-size:20px;color: rgb(255, 255, 255);background-color:red;padding:10px 28px 10px 10px;border-radius:5px'></i>`;
+  listElement.innerHTML = `<label><input type='checkbox' onchange='toggle(event)'><h2>${text}</h2> <span>Deadline:${date}</span></label><i class="fa-solid fa-trash" style='font-size:20px;color: rgb(255, 255, 255);background-color:red;padding:10px 28px 10px 10px;border-radius:5px'></i>`;
 
   list.appendChild(listElement);
 
   let timeout = setTimeout(() => {
     listElement.classList.add("showListElement");
   }, 100);
+}
+
+function toggle(e){
+  if(e.target.checked){e.target.nextElementSibling.classList.add('checkedLi');e.target.nextElementSibling.nextElementSibling.classList.add('checkedLi')}
+  else{e.target.nextElementSibling.classList.remove('checkedLi');e.target.nextElementSibling.nextElementSibling.classList.remove('checkedLi')}
+}
+
+function setError(text,date){
+  if(!text && !date){throw new Error('Neither Name nor Deadline is entered')}
+  if(!text){throw new Error('Name is not entered')}
+  if(!date){throw new Error('Deadline is not entered')}
 }
 
 input.addEventListener("keydown", (e) => {
@@ -42,17 +53,21 @@ list.addEventListener("click", (e) => {
   if (e.target.classList.contains("fa-trash")) {
     e.target.parentElement.classList.remove("showListElement");
     let timeout = setTimeout(() => {
-      e.target.parentElement.remove();
-      saveLocalStorage();
+      e.target.parentElement.parentElement.remove();
+      // saveLocalStorage();
     }, 400);
   }
 });
 
-function saveLocalStorage() {
-  let data = [];
-  list.querySelectorAll("li").forEach((item) => {
-    data.push(item.textContent);
-  });
+form.addEventListener('submit',e=>{
+  e.preventDefault()
+})
 
-  localStorage.setItem("items", JSON.stringify(data));
-}
+// function saveLocalStorage() {
+//   let data = [];
+//   list.querySelectorAll("li").forEach((item) => {
+//     data.push(item.textContent);
+//   });
+
+//   localStorage.setItem("items", JSON.stringify(data));
+// }
